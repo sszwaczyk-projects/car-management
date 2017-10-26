4cecf17af20a15d3d50938e581625bb2f421d001
Sebastian Szwaczyk <sszwaczyk@gmail.com>
2017-10-26 10:38:29 +0000

    Initial commit. Spring boot + thymeleaf app.

---
+++ src/main/java/pl/sszwaczyk/carmanagement/CarManagementApplication.java
@@ -0,0 +1,12 @@
+package pl.sszwaczyk.carmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApplication.class, args);
+	}
+}
